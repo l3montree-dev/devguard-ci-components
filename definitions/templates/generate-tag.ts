505ae1dfbe5d1e957dc657f2cae89f8110d93460
David Luhmer <david.luhmer@l3montree.com>
2026-05-08 10:32:17 +0200

    define filename once

--- definitions/templates/generate-tag.ts
+++ definitions/templates/generate-tag.ts
@@ -43,23 +43,24 @@ export const GenerateTagTemplate = defineJob(GenerateTagJobInputs, (inputValues)
             GIT_STRATEGY: inputValues.git_strategy as any,
             IMAGE_SUFFIX: inputValues.image_suffix,
             DEVGUARD_ARTIFACT_NAME: inputValues.devguard_artifact_name,
+            ENV_FILENAME: `generate_tag_${inputValues.upstream_version}_${inputValues.architecture}.env`,
         },
         image: {
             name: ContainerImages.DEVGUARD_SCANNER,
             pull_policy: inputValues.pull_policy as any,
         },
         script: `echo "Running generate-tag job..."
-devguard-scanner generate-tag --imageSuffix "$IMAGE_SUFFIX" --imageVariant "${inputValues.image_variant}" --architecture "${inputValues.architecture}" --imagePath "${inputValues.image_path}" --ref "$CI_COMMIT_REF_NAME" --upstreamVersion "${inputValues.upstream_version}" >> generate_tag_${inputValues.upstream_version}_${inputValues.architecture}.env
+devguard-scanner generate-tag --imageSuffix "$IMAGE_SUFFIX" --imageVariant "${inputValues.image_variant}" --architecture "${inputValues.architecture}" --imagePath "${inputValues.image_path}" --ref "$CI_COMMIT_REF_NAME" --upstreamVersion "${inputValues.upstream_version}" >> \${ENV_FILENAME}
 echo "Tag generation completed. Generated tags:"
-cat generate_tag_${inputValues.upstream_version}_${inputValues.architecture}.env
-echo 'Filename: generate_tag_${inputValues.upstream_version}_${inputValues.architecture}.env'
+cat \${ENV_FILENAME}
+echo "Filename: \${ENV_FILENAME}"
 ` as any,
         artifacts: {
             reports: {
-                dotenv: `generate_tag_${inputValues.upstream_version}_${inputValues.architecture}.env`
+                dotenv: "${ENV_FILENAME}"
             },
             paths: [
-                `generate_tag_${inputValues.upstream_version}_${inputValues.architecture}.env`
+                "${ENV_FILENAME}"
             ]
         }
     }
