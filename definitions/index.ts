479ad20a7e60a923f31c3c1ddd975789385e8b14
David Luhmer <david.luhmer@l3montree.com>
2026-05-08 13:48:46 +0200

    use correct path for attestation report

--- definitions/index.ts
+++ definitions/index.ts
@@ -55,8 +55,8 @@ const fullAttest = AttestTemplate({
             "predicate_type": "https://slsa.dev/provenance/v1"
         },
         {
-            "source": "./artifacts/attestation-report.sarif",
-            "predicate_type": "https://gitlab.opencode.de/open-code/badgebackend/source-provenance-attestation-service/-/raw/main/schema/source-provenance-schema-1.0.0.json"
+            "source": "./artifacts/attestation-report.json",
+            "predicate_type": "https://gitlab.opencode.de/open-code/badgebackend/source-provenance-attestation-service/-/raw/main/schema/source-provenance-schema-1.0.1.json"
         }
     ] as any
 });
