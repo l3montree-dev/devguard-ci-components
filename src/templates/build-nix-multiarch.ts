7ecad789695e7bb69b2b773b2f7ab3cd9dd285e5
Julian Kepka <julian.kepka@l3montree.com>
2026-06-22 14:03:40 +0200

    fix: changed to test stage

--- src/templates/build-nix-multiarch.ts
+++ src/templates/build-nix-multiarch.ts
@@ -372,7 +372,7 @@ export const BuildNixMultiArchBuildImageTemplate = defineJobGitLab(BuildNixMulti
             devguard_api_url: inputValues.devguard_api_url,
             build_job_name: `devguard:build_oci_image${inputValues.job_suffix}`,
             job_suffix: inputValues.job_suffix,
-            build_stage: "build",
+            build_stage: "test",
             attest_stage: "test",
             image_suffix: inputValues.image_suffix,
             architecture: "${ARCHITECTURE}",
